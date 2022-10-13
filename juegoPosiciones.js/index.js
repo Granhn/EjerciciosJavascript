bbb45f76d4bbca73c833676a0e88898f1fe4bb20
Granhn <fuenzaladeveloper@gmail.com>
2022-10-13 12:42:02 -0300

    Creacion de la card lista. Falta funcionalidad botones

---
+++ juegoPosiciones.js/index.js
@@ -0,0 +1,23 @@
+const d = document,
+$button1 = d.getElementById("boton1"),
+$button2 = d.getElementById("boton2"),
+$button3 = d.getElementById("boton3");
+
+//Creating the card 
+const $newCard = d.createElement("figure"),
+$img = d.createElement("img"),
+$figcaption = d.createElement("figcaption"),
+$figtext = d.createTextNode("Any");
+
+
+//Setting clases and attributes
+$img.setAttribute("src","https://placeimg.com/200/200/any");
+$img.setAttribute("alt","Any");
+$figcaption.appendChild($figtext);
+
+//Adding the nodes to the new card
+
+$newCard.appendChild($img);
+$newCard.appendChild($figcaption);
+$newCard.classList.add("card");
+
